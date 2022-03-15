0e746a293e4cb7ff5545bb9e1679a83ea1a285fc
Wilmer Zwietering <wzwietering@gmail.com>
2022-03-15 11:09:10 +0100

    Explicit import, rename source folder

--- backend/team-proj-abbr/lib-core/Matcher.hs
+++ backend/team-proj-abbr/lib-core/Matcher.hs
@@ -1,7 +1,7 @@
 module Matcher where
 
-import           KnowledgeBase
-import           Parser
+import           KnowledgeBase (KnowledgeBaseStructure)
+import           Parser (ParseStructure)
 
 match :: KnowledgeBaseStructure -> ParseStructure -> ParseStructure
 match = undefined
