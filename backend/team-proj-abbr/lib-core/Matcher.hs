cd33e2c10de954b39448070eac5994d45ee159e8
wzwietering <wzwietering@gmail.com>
2022-03-16 13:00:15 +0100

    Stylish Haskell feedback

--- backend/team-proj-abbr/lib-core/Matcher.hs
+++ backend/team-proj-abbr/lib-core/Matcher.hs
@@ -1,7 +1,7 @@
 module Matcher where
 
 import           KnowledgeBase (KnowledgeBaseStructure)
-import           Parser (ParseStructure)
+import           Parser        (ParseStructure)
 
 match :: KnowledgeBaseStructure -> ParseStructure -> ParseStructure
 match = undefined
