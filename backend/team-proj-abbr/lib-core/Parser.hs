b50f2288f975b8f45108de45eb050c9631b81ce7
wzwietering <wzwietering@gmail.com>
2022-03-23 14:03:32 +0100

    Updated token type

--- backend/team-proj-abbr/lib-core/Parser.hs
+++ backend/team-proj-abbr/lib-core/Parser.hs
@@ -1,7 +1,11 @@
 module Parser where
 
-newtype Token
-  = Token String
+-- | We parse a string into a NoToken if it does not match the syntax for
+-- | parsing. The DoMap is a string that has to be mapped. In our proposals,
+-- | the DoMap strings start with @@
+data Token
+  = NoToken String
+  | DoMap String
   deriving (Eq, Ord, Show)
 
 type ParseStructure
@@ -9,4 +13,4 @@ type ParseStructure
 
 -- | Map a string to a list of Tokens
 parseInput :: String -> ParseStructure
-parseInput s = map Token (words s)
+parseInput s = map NoToken (words s)
