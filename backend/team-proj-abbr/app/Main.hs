ef7cb56437edc3ee2b9c580a7444a74e7aa36d9b
Dmitrii Orlov <dmtree.dev@yahoo.com>
2022-03-24 15:33:39 +0100

    Very basic example of optparse

--- backend/team-proj-abbr/app/Main.hs
+++ backend/team-proj-abbr/app/Main.hs
@@ -1,14 +1,49 @@
+{-#LANGUAGE ScopedTypeVariables#-}
 module Main where
 
-import           LibCore.Decoder
-import           LibCore.InputInterface
-import           LibCore.KnowledgeBase
-import           LibCore.Mapper
-import           LibCore.Matcher
-import           LibCore.OutputInterface
-import           LibCore.Parser
+import Options.Applicative
+
+data Opts = Opts
+    { optGlobalFlag :: !Bool
+    , optCommand :: !Command
+    }
+
+data Command
+    = Create String
+    | Delete
 
 main :: IO ()
-main = returnOutput $ decode $ mapParseStructure k $ match k $ parse getInput
+main = do
+    (opts :: Opts) <- execParser optsParser
+    case optCommand opts of
+        Create name -> putStrLn ("Created the thing named " ++ name)
+        Delete -> putStrLn "Deleted the thing!"
+    putStrLn ("global flag: " ++ show (optGlobalFlag opts))
   where
-    k = getKnowledgeBase
+    optsParser :: ParserInfo Opts
+    optsParser =
+        info
+            (helper <*> versionOption <*> programOptions)
+            (fullDesc <> progDesc "optparse subcommands example" <>
+             header
+                 "optparse-sub-example - a small example program for optparse-applicative with subcommands")
+    versionOption :: Parser (a -> a)
+    versionOption = infoOption "0.0" (long "version" <> help "Show version")
+    programOptions :: Parser Opts
+    programOptions =
+        Opts <$> switch (long "global-flag" <> help "Set a global flag") <*>
+        hsubparser (createCommand <> deleteCommand)
+    createCommand :: Mod CommandFields Command
+    createCommand =
+        command
+            "create"
+            (info createOptions (progDesc "Create a thing"))
+    createOptions :: Parser Command
+    createOptions =
+        Create <$>
+        strArgument (metavar "NAME" <> help "Name of the thing to create")
+    deleteCommand :: Mod CommandFields Command
+    deleteCommand =
+        command
+            "delete"
+            (info (pure Delete) (progDesc "Delete the thing"))
