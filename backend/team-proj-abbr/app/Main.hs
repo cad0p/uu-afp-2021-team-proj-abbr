b36d3fddc9f1f0cd40ce8d363a7f5f957e226c02
Dmitrii Orlov <dmtree.dev@yahoo.com>
2022-03-24 13:10:09 +0100

    Sample CLI interface setup with cmdargs

--- backend/team-proj-abbr/app/Main.hs
+++ backend/team-proj-abbr/app/Main.hs
@@ -1,14 +1,44 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 module Main where
 
-import           LibCore.Decoder
-import           LibCore.InputInterface
-import           LibCore.KnowledgeBase
-import           LibCore.Mapper
-import           LibCore.Matcher
-import           LibCore.OutputInterface
-import           LibCore.Parser
+import           System.Console.CmdArgs
 
+projectName :: String
+projectName = "shrthdnr"
+
+-- |ShortHndr CLI interface specification.
+data ShortHndr
+  = ExpandBulk
+      { src     :: Maybe FilePath
+      , out     :: Maybe FilePath
+      , inplace :: Maybe Bool
+      }
+  | AddRecord
+      { keyword   :: String
+      , expansion :: String
+      , src       :: Maybe FilePath
+      }
+  deriving (Data, Show, Typeable)
+
+fileFlags :: String -> Maybe FilePath -> Maybe FilePath
+fileFlags h f = f &= help h &= typFile
+
+expandBulk :: ShortHndr
+expandBulk = ExpandBulk
+    {src = fileFlags "Source file" (pure "shorthndr-input.txt")
+    ,out = fileFlags "Output file" (pure "shorthndr--out.txt")
+    ,inplace = def
+    } &= help "Expand the abreviations in the provided file"
+
+addRecord :: ShortHndr
+addRecord = AddRecord
+    {keyword = def
+    ,expansion = def
+    ,src = fileFlags "KnowledgeBase source file" (pure "shorthndr-kb.csv")
+    } &= help "Add a new abbreviation record to the Knowledge Base"
+
+-- |Main entrypoint of the CLI application.
 main :: IO ()
-main = returnOutput $ decode $ mapParseStructure k $ match k $ parse getInput
-  where
-    k = getKnowledgeBase
+main = mockCliHandler =<< cmdArgs (modes [expandBulk, addRecord])
+  where mockCliHandler = print
+
