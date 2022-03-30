b191992cca661e27f9e07997aa7152e90a126af3
Dmitrii Orlov <dmtree.dev@yahoo.com>
2022-03-30 16:05:48 +0200

    extend documentation on `cliMain`

--- backend/team-proj-abbr/lib-cli/LibCli/CliMain.hs
+++ backend/team-proj-abbr/lib-cli/LibCli/CliMain.hs
@@ -22,13 +22,22 @@ mockCliHandler c@CS.Delete{}  = print $ "deleting! --> " ++ show c
 
 -- |Main entrypoint of the CLI application.
 --
--- _CLI Endpoint design:_
+-- /CLI endpoints:/
 --
---     * replace    - expand all the abbreviation in the full text file
---     * expand     - find an expansion for a single abbreviation input
---     * list       - list all the known expansion records
---     * add        - add a new record to the knowledge base
---     * update     - modify an existing record in the knowledge base
---     * delete     - delete an existing record from the knowledge base
+--    * replace   - expand all the abbreviation in the full text file
+--    * expand    - find an expansion for a single abbreviation input
+--    * list      - list all the known expansion records
+--    * add       - add a new record to the knowledge base
+--    * update    - modify an existing record in the knowledge base
+--    * delete    - delete an existing record from the knowledge base
+--
+-- /Usage:/
+--
+--    * --help                            - prints the full CLI tool support
+--    * \<command\> [args\*] [options\*]  - invokes the command
+--
+-- /See more:/
+--
+--    * See 'LibCli.CliSpec' for more information about the CLI endpoints.
 cliMain :: IO ()
 cliMain = mockCliHandler =<< CMD.cmdArgs (CMD.modes CS.cliModes)
