52d264fef69174fbdc54be5badb140df3342fd88
Dmitrii Orlov <dmtree.dev@yahoo.com>
2022-03-30 14:12:08 +0200

    expand the main function haddock comment

--- backend/team-proj-abbr/app/Main.hs
+++ backend/team-proj-abbr/app/Main.hs
@@ -38,6 +38,14 @@ addRecord = AddRecord
     } &= help "Add a new abbreviation record to the Knowledge Base"
 
 -- |Main entrypoint of the CLI application.
+--
+-- Endpoint design:
+--      - expand - expand all the abbreviation in the full text file
+--      - lookup - find an expansion for a single abbreviation input
+--      - list   - list all the known expansion records
+--      - add    - add a new record to the knowledge base
+--      - update - modify an existing record in the knowledge base
+--      - delete - delete an existing record from the knowledge base
 main :: IO ()
 main = mockCliHandler =<< cmdArgs (modes [expandBulk, addRecord])
   where mockCliHandler = print
