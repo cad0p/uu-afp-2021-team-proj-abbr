a71397b5d9f1cc0d96ff8c3867069c8a2a9a340c
Wilmer Zwietering <wzwietering@gmail.com>
2022-03-30 17:50:21 +0200

    Parse input string into a ParseStructure

--- backend/team-proj-abbr/lib-core/LibCore/Parser.hs
+++ backend/team-proj-abbr/lib-core/LibCore/Parser.hs
@@ -1,10 +1,57 @@
 module LibCore.Parser where
 
-import           LibCore.Models (Token (NoToken))
+import           Data.Functor
+import           LibCore.Models     (Keyword (Keyword), Token (DoMap, NoToken))
+import           Text.Parsec
+    ( ParseError
+    , alphaNum
+    , choice
+    , many
+    , many1
+    , oneOf
+    , parse
+    , string
+    )
+import           Text.Parsec.String (Parser)
 
 type ParseStructure
   = [Token]
 
+-- | The abbreviation symbol. Can later be made configurable
+abbSymbol :: String
+abbSymbol = "@@"
+
+-- | Given a string, parse it. This function can throw an error if parsing fails
+doParse :: String -> ParseStructure
+doParse s = case parseInput s of
+  Left err -> error $ show err
+  Right ps -> ps
+
 -- | Map a string to a list of Tokens
-parseInput :: String -> ParseStructure
-parseInput s = map NoToken (words s)
+parseInput :: String -> Either ParseError ParseStructure
+parseInput = parse (mainParser abbSymbol) ""
+
+-- | The main parser tries to consume all input into a ParseStructure, given an abbreviation symbol
+mainParser :: String -> Parser ParseStructure
+mainParser s = do many $ choice [lexeme $ abbrParser s, lexeme noAbbrParser]
+
+-- | Given an abbreviation string s, parse the string after it
+abbrParser :: String -> Parser Token
+abbrParser s = do
+  void $ string s
+  a <- many1 alphaNum
+  return $ DoMap $ Keyword a
+
+-- | Parse any string into a token
+noAbbrParser :: Parser Token
+noAbbrParser = do
+  a <- many1 alphaNum
+  return $ NoToken a
+
+-- | Consume whitespace characters
+whitespace :: Parser ()
+whitespace = void $ many $ oneOf [' ', '\n', '\t']
+
+-- | Lexeme parsers consume the whitespace after them
+lexeme :: Parser a -> Parser a
+lexeme p = p <* whitespace
