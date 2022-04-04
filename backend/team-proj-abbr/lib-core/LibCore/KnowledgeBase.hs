b8b5aaa1ea1341a0516867295337b16c5c0c1382
Pier Carlo Cadoppi <p.c.cadoppi@students.uu.nl>
2022-04-04 17:25:17 +0200

    first commit, add cassava import

--- backend/team-proj-abbr/lib-core/LibCore/KnowledgeBase.hs
+++ backend/team-proj-abbr/lib-core/LibCore/KnowledgeBase.hs
@@ -1,5 +1,6 @@
 {-|
-Description : TODO: defines the knowledge base
+Description : Defines the knowledge base, and a way to parse it from a file.
+              For now, only '.csv' is supported
 Copyright   : Copyright (c) 2022 Pier Carlo Cadoppi, Dmitrii Orlov, Wilmer Zwietering
 License     : BSD3
 Maintainer  : p.c.cadoppi@students.uu.nl; d.orlov@student.tue.nl; w.j.zwietering@students.uu.nl
