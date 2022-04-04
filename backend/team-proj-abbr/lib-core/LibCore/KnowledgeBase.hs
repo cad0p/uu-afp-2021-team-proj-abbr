5e3d36fb63e9cd080792ed14fa801ffcae50d164
Pier Carlo Cadoppi <p.c.cadoppi@students.uu.nl>
2022-04-04 19:14:15 +0200

    ** does not work ** getKnowledgeBase idea

--- backend/team-proj-abbr/lib-core/LibCore/KnowledgeBase.hs
+++ backend/team-proj-abbr/lib-core/LibCore/KnowledgeBase.hs
@@ -7,13 +7,48 @@ Maintainer  : p.c.cadoppi@students.uu.nl; d.orlov@student.tue.nl; w.j.zwietering
 Stability   : experimental
 -}
 
+{-# LANGUAGE OverloadedStrings #-}
+
+
 module LibCore.KnowledgeBase where
 
-import           Data.Map       (Map, empty)
-import           LibCore.Models (Keyword)
+import           Control.Applicative  hiding (empty)
+import qualified Data.ByteString.Lazy as BL
+import           Data.Csv
+import           Data.Map             (Map, empty)
+import qualified Data.Vector          as V
+import           LibCore.Models       (Keyword (Keyword, keyword, plural))
+
 
 
 type KnowledgeBaseStructure = Map Keyword Keyword
 
-getKnowledgeBase :: FilePath -> KnowledgeBaseStructure
-getKnowledgeBase _ = empty
+
+data KbCsvEntry
+  = KbCsvEntry
+      { abbreviation :: String
+      , expansion    :: String
+      }
+
+instance FromNamedRecord KbCsvEntry where
+  parseNamedRecord r = KbCsvEntry <$> r .: "abbreviation" <*> r .: "expansion"
+
+{- 'getKnowledgeBase' parses from a CSV file to a 'KnowledgeBaseStructure'
+
+-}
+getKnowledgeBase
+  :: FilePath -- ^ it is assumed to be a valid path to an existing KB with the format "*.csv"
+  -> KnowledgeBaseStructure
+getKnowledgeBase fp = do
+  kbs     <- (empty :: Map Keyword Keyword)
+  csvData <- BL.readFile fp
+  case decodeByName csvData of
+    Left  err    -> error ("decoding" ++ err)
+    Right (_, v) -> V.forM_
+      v
+      (\e -> do-- e stands for kb entry
+        insert (Keyword { keyword = abbreviation e, plural = False })
+               (Keyword { keyword = expansion e, plural = False })
+               kbs
+        kbs
+      )
