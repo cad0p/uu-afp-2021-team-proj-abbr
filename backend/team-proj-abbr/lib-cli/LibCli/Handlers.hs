49545a8195766cf5383988dda9a92743165bc984
Dmitrii Orlov <dmtree.dev@yahoo.com>
2022-04-05 18:36:36 +0200

    Introduce a new replace handler skeleton

--- backend/team-proj-abbr/lib-cli/LibCli/Handlers.hs
+++ backend/team-proj-abbr/lib-cli/LibCli/Handlers.hs
@@ -7,6 +7,22 @@ Stability   : experimental
 -}
 
 module LibCli.Handlers where
+import           LibCli.OutputInterface (returnOutput)
+import           LibCore.Decoder        as D (decode)
+import           LibCore.KnowledgeBase  (KnowledgeBaseStructure)
+import           LibCore.Mapper         as M (mapParseStructure)
+import           LibCore.Models         (Error (..))
+import           LibCore.Parser         as P (doParse)
+
+---------------
+-- Utilities --
+---------------
+
+-- | Wraps the file loading in a predefined error handling.
+readWrapper :: (FilePath -> a) -> Maybe FilePath -> Either Error a
+readWrapper f mfp = case mfp of
+  Nothing -> Left $ StandardError "no file path provided."
+  Just s  -> Right $ f s
 
 --------------------------
 -- Expansion Operations --
@@ -14,7 +30,33 @@ module LibCli.Handlers where
 
 -- TODO:
 -- expand
--- replace
+
+-- | Replace command handler.
+replaceHandler
+  :: Maybe FilePath -- ^ KB file path
+  -> Maybe FilePath -- ^ Input file path
+  -> Maybe FilePath -- ^ Output file path
+  -> IO () -- ^ Writes the modified file out to the specified location
+replaceHandler kbfp ifp ofp = do
+  case process of
+    Left  err -> error $ show err
+    Right s   -> writeOutput ofp s
+ where
+  process :: Either Error String
+  process = exec <$> loadKb kbfp <*> loadInput ifp
+
+  loadKb :: Maybe FilePath -> Either Error KnowledgeBaseStructure
+  loadKb = const $ Left $ StandardError "not implemented KB"
+
+  loadInput :: Maybe FilePath -> Either Error String
+  loadInput = const $ Left $ StandardError "not implemented INPUT"
+
+  exec :: KnowledgeBaseStructure -> String -> String
+  exec kb s = D.decode $ M.mapParseStructure kb $ P.doParse s
+
+  writeOutput :: Maybe FilePath -> String -> IO ()
+  writeOutput = returnOutput
+
 
 -------------------------
 -- Knowledge Base CRUD --
