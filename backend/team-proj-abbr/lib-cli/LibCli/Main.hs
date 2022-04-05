8d2a65627948b12a0b24b355c8c00e8525685b59
Dmitrii Orlov <dmtree.dev@yahoo.com>
2022-04-05 10:44:53 +0200

    rename mockCliHandler to cliController

--- backend/team-proj-abbr/lib-cli/LibCli/Main.hs
+++ backend/team-proj-abbr/lib-cli/LibCli/Main.hs
@@ -29,14 +29,13 @@ import           System.Directory        (doesFileExist)
 
 -- TODO(tech-debt): define a typeclass for the modes instead of the pattern matching
 -- TODO: (future task) implement the actual handlers with the business logic.
-mockCliHandler :: CS.ShortHndr -> IO ()
--- mockCliHandler c@CS.Replace{} = print $ "replacing! --> " ++ show c
-mockCliHandler c@CS.Replace{} = replaceMode c
-mockCliHandler c@CS.Expand{}  = print $ "expanding! --> " ++ show c
-mockCliHandler c@CS.List{}    = print $ "listing! --> " ++ show c
-mockCliHandler c@CS.Add{}     = print $ "adding! --> " ++ show c
-mockCliHandler c@CS.Update{}  = print $ "updating! --> " ++ show c
-mockCliHandler c@CS.Delete{}  = print $ "deleting! --> " ++ show c
+cliController :: CS.ShortHndr -> IO ()
+cliController c@CS.Replace{} = replaceMode c
+cliController c@CS.Expand{}  = print $ "expanding! --> " ++ show c
+cliController c@CS.List{}    = print $ "listing! --> " ++ show c
+cliController c@CS.Add{}     = print $ "adding! --> " ++ show c
+cliController c@CS.Update{}  = print $ "updating! --> " ++ show c
+cliController c@CS.Delete{}  = print $ "deleting! --> " ++ show c
 
 
 {-| 'replaceMode' does the replacind heavy lifting
@@ -63,7 +62,7 @@ replaceMode c@CS.Replace{} = do
               Right (_, kb_v) -> returnOutput
                 (out c)
                 (decode (mapParseStructure (getKnowledgeBase kb_v) (doParse s)))
--- Impossible case because of the mockCliHandler
+-- Impossible case because of the cliController
 replaceMode _ = undefined
 
 ----------------------------
@@ -90,4 +89,4 @@ replaceMode _ = undefined
 --
 --    * See 'LibCli.Spec' for more information about the CLI endpoints.
 cliMain :: IO ()
-cliMain = mockCliHandler =<< CMD.cmdArgs (CMD.modes CS.cliModes)
+cliMain = cliController =<< CMD.cmdArgs (CMD.modes CS.cliModes)
