26c2752854e5cd7eee8c50596cefe840145e2fea
Dmitrii Orlov <dmtree.dev@yahoo.com>
2022-04-05 09:38:04 +0200

    quick listAll implementation

--- backend/team-proj-abbr/lib-core/LibCore/KnowledgeBase.hs
+++ backend/team-proj-abbr/lib-core/LibCore/KnowledgeBase.hs
@@ -8,8 +8,8 @@ Stability   : experimental
 
 module LibCore.KnowledgeBase where
 
-import           Data.Map       (Map, empty)
-import           LibCore.Models (Keyword)
+import           Data.Map       (Map, empty, toList)
+import           LibCore.Models (Error (StandardError), Keyword (..))
 
 type KnowledgeBaseStructure = Map Keyword Keyword
 
@@ -19,19 +19,26 @@ getKnowledgeBase = empty
 buildKnowledgeBase :: [Keyword] -> KnowledgeBaseStructure
 buildKnowledgeBase _ = undefined
 
-list :: KnowledgeBaseStructure -> [Keyword]
-list _ = []
+-- |Get the list of all the stored records in the Knowledge Base.
+--
+-- Examples:
+--
+-- >>> testKB = Data.Map.fromList [(Keyword {keyword = "brb", plural = False}, Keyword {keyword = "be right back", plural = False})]
+-- >>> listAll testKB
+-- [(Keyword {keyword = "brb", plural = False},Keyword {keyword = "be right back", plural = False})]
+listAll :: KnowledgeBaseStructure -> [(Keyword, Keyword)]
+listAll = toList
 
 get :: KnowledgeBaseStructure -> Keyword -> Either Error Keyword
-get _ _ = Left "fail"
+get _ _ = Left $ StandardError "fail"
 
 add :: KnowledgeBaseStructure -> Keyword -> Either Error Keyword
-add _ _ = Left "fail"
+add _ _ = Left $ StandardError "fail"
 
 update :: KnowledgeBaseStructure -> Keyword -> Either Error Keyword
-update _ _ = Left "fail"
+update _ _ = Left $ StandardError "fail"
 
 delete :: KnowledgeBaseStructure -> Keyword -> Either Error Bool
-delete _ _ = Left "fail"
+delete _ _ = Left $ StandardError "fail"
 
 
