c0bd85345aa519720f9a9f43ef55aa4f1b4bcc7f
Dmitrii Orlov <dmtree.dev@yahoo.com>
2022-04-05 10:14:33 +0200

    simple put implementation

--- backend/team-proj-abbr/lib-core/LibCore/KnowledgeBase.hs
+++ backend/team-proj-abbr/lib-core/LibCore/KnowledgeBase.hs
@@ -1,5 +1,5 @@
 {-|
-Description : TODO: defines the knowledge base
+Description : The knowledge base implementation.
 Copyright   : Copyright (c) 2022 Pier Carlo Cadoppi, Dmitrii Orlov, Wilmer Zwietering
 License     : BSD3
 Maintainer  : p.c.cadoppi@students.uu.nl; d.orlov@student.tue.nl; w.j.zwietering@students.uu.nl
@@ -15,9 +15,11 @@ import           Data.Map       as M
     , insert
     , lookup
     , toList
+    , update
     )
 import           LibCore.Models (Error (StandardError), Keyword (..))
 
+-- TODO(future focus): support partial updates and state? RWS monad?
 type KnowledgeBaseStructure = M.Map Keyword Keyword
 
 getKnowledgeBase :: KnowledgeBaseStructure
@@ -70,8 +72,26 @@ add
   -> (Keyword, KnowledgeBaseStructure)
 add kb k r = (r, M.insert k r kb)
 
-update :: KnowledgeBaseStructure -> Keyword -> Either Error Keyword
-update _ _ = Left $ StandardError "fail"
+-- |Updates the record at given key with the new value.
+-- Only full updates are supported.
+-- If the key is not in the KB, returns an error.
+--
+-- Examples:
+-- >>> testKB = buildKnowledgeBase [(Keyword {keyword = "brb", plural = False}, Keyword {keyword = "be right back", plural = False})]
+-- >>> put testKB (Keyword {keyword = "brb", plural = False}) (Keyword {keyword = "BE RIGHT BACK", plural = False})
+-- Right (Keyword {keyword = "BE RIGHT BACK", plural = False},fromList [(Keyword {keyword = "brb", plural = False},Keyword {keyword = "BE RIGHT BACK", plural = False})])
+--
+-- >>> put testKB (Keyword {keyword = "btw", plural = False}) (Keyword {keyword = "by the way!", plural = False})
+-- Left (StandardError "no record found for this keyword")
+put
+  :: KnowledgeBaseStructure
+  -> Keyword
+  -> Keyword
+  -> Either Error (Keyword, KnowledgeBaseStructure)
+put kb k r = case get kb k of
+  Left  er -> Left er
+  Right _  -> Right (r, M.update (\_ -> Just r) k kb)
+
 
 delete :: KnowledgeBaseStructure -> Keyword -> Either Error Bool
 delete _ _ = Left $ StandardError "fail"
