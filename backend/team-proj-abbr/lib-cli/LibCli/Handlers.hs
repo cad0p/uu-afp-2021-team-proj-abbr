ad7a4280842a4f9bd704bc278f336856cda671e5
Dmitrii Orlov <dmtree.dev@yahoo.com>
2022-04-05 20:29:42 +0200

    WIP: reusable handler blocks

--- backend/team-proj-abbr/lib-cli/LibCli/Handlers.hs
+++ backend/team-proj-abbr/lib-cli/LibCli/Handlers.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 {-|
 Description : Environment agnostic command handlers for the CLI.
 Copyright   : Copyright (c) 2022 Pier Carlo Cadoppi, Dmitrii Orlov, Wilmer Zwietering
@@ -7,22 +8,32 @@ Stability   : experimental
 -}
 
 module LibCli.Handlers where
+import           Control.Applicative    (Alternative (empty))
+import qualified Data.ByteString.Lazy   as BL
+import           Data.Csv               (decodeByName)
+import           Data.Maybe             (fromMaybe)
+import           LibCli.Adapters        (getKnowledgeBase)
 import           LibCli.OutputInterface (returnOutput)
 import           LibCore.Decoder        as D (decode)
 import           LibCore.KnowledgeBase  (KnowledgeBaseStructure)
 import           LibCore.Mapper         as M (mapParseStructure)
 import           LibCore.Models         (Error (..))
 import           LibCore.Parser         as P (doParse)
+import           System.Directory       (doesFileExist)
 
 ---------------
 -- Utilities --
 ---------------
 
--- | Wraps the file loading in a predefined error handling.
-readWrapper :: (FilePath -> a) -> Maybe FilePath -> Either Error a
-readWrapper f mfp = case mfp of
-  Nothing -> Left $ StandardError "no file path provided."
-  Just s  -> Right $ f s
+-- | Wraps the file reading in a predefined error handling.
+-- If the file path was not provided, returns Nothing.
+-- Otherwise, executes the given function.
+readWrapper
+  :: (FilePath -> a) -- ^ File access function
+  -> Maybe FilePath  -- ^ File path
+  -> Maybe a         -- ^ Operation result
+readWrapper f mfp = f <$> mfp
+
 
 --------------------------
 -- Expansion Operations --
@@ -37,22 +48,43 @@ replaceHandler
   -> Maybe FilePath -- ^ Input file path
   -> Maybe FilePath -- ^ Output file path
   -> IO () -- ^ Writes the modified file out to the specified location
-replaceHandler kbfp ifp ofp = do
-  case process of
+replaceHandler kbfp inpfp ofp = do
+  let kbp = fromMaybe "" kbfp
+  let inp = fromMaybe "" inpfp
+  kb_exists  <- doesFileExist kbp
+  inp_exists <- doesFileExist inp
+  res        <- process (kb_exists, kbp) (inp_exists, inp)
+  case res of
     Left  err -> error $ show err
     Right s   -> writeOutput ofp s
  where
-  process :: Either Error String
-  process = exec <$> loadKb kbfp <*> loadInput ifp
+  process :: (Bool, FilePath) -> (Bool, FilePath) -> IO (Either Error String)
+  process (False, p) _ = do
+    return $ Left $ StandardError $ "KB file not found at " ++ p
+  process _ (False, p) = do
+    return $ Left $ StandardError $ "Input file not found at " ++ p
+  process (_, kbp) (_, inp) = do
+    lkb <- loadKb kbp
+    lin <- loadInput inp
+    case exec <$> lkb <*> lin of
+      Left  er  -> return $ Left $ StandardError $ show er
+      Right ios -> Right <$> ios
 
-  loadKb :: Maybe FilePath -> Either Error KnowledgeBaseStructure
-  loadKb = const $ Left $ StandardError "not implemented KB"
+  loadKb :: FilePath -> IO (Either Error KnowledgeBaseStructure)
+  loadKb p = do
+    putStrLn p
+    csvData <- BL.readFile p
+    case decodeByName csvData of
+      Left  s      -> return $ Left $ StandardError s
+      Right (_, v) -> return $ Right $ getKnowledgeBase v
 
-  loadInput :: Maybe FilePath -> Either Error String
-  loadInput = const $ Left $ StandardError "not implemented INPUT"
+  loadInput :: FilePath -> IO (Either Error String)
+  loadInput _ = do
+    return $ Left $ StandardError "Load input is not implemented"
 
-  exec :: KnowledgeBaseStructure -> String -> String
-  exec kb s = D.decode $ M.mapParseStructure kb $ P.doParse s
+  exec :: KnowledgeBaseStructure -> String -> IO String
+  exec kb s = do
+    return $ D.decode $ M.mapParseStructure kb $ P.doParse s
 
   writeOutput :: Maybe FilePath -> String -> IO ()
   writeOutput = returnOutput
