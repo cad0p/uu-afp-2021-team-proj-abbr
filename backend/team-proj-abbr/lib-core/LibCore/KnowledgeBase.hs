8dee807fa5fbac0d0ca907b4fc344256d6c7113e
Dmitrii Orlov <dmtree.dev@yahoo.com>
2022-04-05 10:20:25 +0200

    Simple delete implementation + add error info in get

--- backend/team-proj-abbr/lib-core/LibCore/KnowledgeBase.hs
+++ backend/team-proj-abbr/lib-core/LibCore/KnowledgeBase.hs
@@ -10,6 +10,7 @@ module LibCore.KnowledgeBase where
 
 import           Data.Map       as M
     ( Map
+    , delete
     , empty
     , fromList
     , insert
@@ -47,11 +48,12 @@ listAll = M.toList
 -- Right (Keyword {keyword = "be right back", plural = False})
 --
 -- >>> get testKB (Keyword {keyword = "beb", plural = False})
--- Left (StandardError "no record found for this keyword")
+-- Left (StandardError "no record found for this keyword : Keyword {keyword = \"beb\", plural = False}")
 get :: KnowledgeBaseStructure -> Keyword -> Either Error Keyword
 get kb k = case M.lookup k kb of
-  Nothing -> Left $ StandardError "no record found for this keyword"
-  Just x  -> Right x
+  Nothing ->
+    Left $ StandardError $ "no record found for this keyword : " ++ show k
+  Just x -> Right x
 
 -- |Add new item to the Knowledge Base.
 -- Adds the item to the KB as a new record if the key was notseen before,
@@ -77,12 +79,13 @@ add kb k r = (r, M.insert k r kb)
 -- If the key is not in the KB, returns an error.
 --
 -- Examples:
+--
 -- >>> testKB = buildKnowledgeBase [(Keyword {keyword = "brb", plural = False}, Keyword {keyword = "be right back", plural = False})]
 -- >>> put testKB (Keyword {keyword = "brb", plural = False}) (Keyword {keyword = "BE RIGHT BACK", plural = False})
 -- Right (Keyword {keyword = "BE RIGHT BACK", plural = False},fromList [(Keyword {keyword = "brb", plural = False},Keyword {keyword = "BE RIGHT BACK", plural = False})])
 --
 -- >>> put testKB (Keyword {keyword = "btw", plural = False}) (Keyword {keyword = "by the way!", plural = False})
--- Left (StandardError "no record found for this keyword")
+-- Left (StandardError "no record found for this keyword : Keyword {keyword = \"btw\", plural = False}")
 put
   :: KnowledgeBaseStructure
   -> Keyword
@@ -93,7 +96,21 @@ put kb k r = case get kb k of
   Right _  -> Right (r, M.update (\_ -> Just r) k kb)
 
 
-delete :: KnowledgeBaseStructure -> Keyword -> Either Error Bool
-delete _ _ = Left $ StandardError "fail"
+-- |Removes an existing item at the given key form the Knowledge Base.
+-- If the key is not present, return as error.
+--
+-- Examples:
+--
+-- >>> testKB = buildKnowledgeBase [(Keyword {keyword = "brb", plural = False}, Keyword {keyword = "be right back", plural = False})]
+-- >>> remove testKB (Keyword {keyword = "brb", plural = False})
+-- Right (fromList [])
+--
+-- >>> remove testKB (Keyword {keyword = "btw", plural = False})
+-- Left (StandardError "no record found for this keyword : Keyword {keyword = \"btw\", plural = False}")
+remove
+  :: KnowledgeBaseStructure -> Keyword -> Either Error KnowledgeBaseStructure
+remove kb k = case get kb k of
+  Left  er -> Left er
+  Right _  -> Right $ M.delete k kb
 
 
