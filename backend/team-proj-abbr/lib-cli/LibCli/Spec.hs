27a1a7e4a71d110146d506ee15d09c2fadd180f0
Pier Carlo Cadoppi <p.c.cadoppi@students.uu.nl>
2022-04-07 12:19:19 +0200

    Revert "redo commit on changed datatype inplace ca5d9f8027094c1d7084694783d17e04e38f20e0"
    This reverts commit 7abf0442ce57744258ce0e1fd2a4a9e858b5f821.

--- backend/team-proj-abbr/lib-cli/LibCli/Spec.hs
+++ backend/team-proj-abbr/lib-cli/LibCli/Spec.hs
@@ -25,9 +25,7 @@ data ShortHndr
   = Replace
       { input   :: Maybe FilePath
       , out     :: Maybe FilePath
-      , inplace :: Maybe FilePath
-        -- ^ this argument replaces input and output with what is provided here,
-        -- and has precedence over the others
+      , inplace :: Maybe Bool
       , kb      :: Maybe FilePath
       }
   -- |Defines the arguments for the expand command
@@ -73,16 +71,11 @@ defaultKB = "data/shorthndr-kb.csv"
 
 replace :: ShortHndr
 replace =
-  Replace
-      { input   = fileFlags "Source file" (pure "shorthndr-input.txt")  -- TODO: rename to avoid -i clash with --inplace
-      , out     = fileFlags "Output file" (pure "shorthndr--out.txt")
-      , kb      = fileFlags "Knowledge Base source file" (pure defaultKB)
-      , inplace = fileFlags
-        ("Source (and Output) file: this argument replaces input and output with"
-        ++ " what is provided here, and has precedence over the others"
-        )
-        (pure "shorthndr-inout.txt")
-      }
+  Replace { input   = fileFlags "Source file" (pure "shorthndr-input.txt")  -- TODO: rename to avoid -i clash with --inplace
+          , out     = fileFlags "Output file" (pure "shorthndr--out.txt")
+          , kb      = fileFlags "Knowledge Base source file" (pure defaultKB)
+          , inplace = CMD.def
+          }
     CMD.&= CMD.help
              "Replace all abreviations in the provided file with their expansions"
 
