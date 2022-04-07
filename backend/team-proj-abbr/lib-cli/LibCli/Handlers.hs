0bf00652a3d4dbaf03991f7e38dde98c68294b09
Dmitrii Orlov <dmtree.dev@yahoo.com>
2022-04-07 20:38:20 +0200

    chore(output-interface-to-handler-utils): no module for 1 function  (#38)
    * Attempt to reorganize outputInterface to make less modules
    
    * Remove OutputInterface module
    
    * White space and comments
    
    * White space
    
    * Move all util functions to a separate module
    
    * Fix handlers module:
    - imports
    - comments

--- backend/team-proj-abbr/lib-cli/LibCli/Handlers.hs
+++ backend/team-proj-abbr/lib-cli/LibCli/Handlers.hs
@@ -1,5 +1,5 @@
 {-|
-Description : Environment agnostic command handlers for the CLI.
+Description : CLI framework agnostic command handlers for the CLI.
 Copyright   : Copyright (c) 2022 Pier Carlo Cadoppi, Dmitrii Orlov, Wilmer Zwietering
 License     : BSD3
 Maintainer  : p.c.cadoppi@students.uu.nl; d.orlov@student.tue.nl; w.j.zwietering@students.uu.nl
@@ -12,34 +12,30 @@ module LibCli.Handlers
   (
   -- $intro
 
-  -- * Utilities
-    loadKb
-  , loadInput
-  , doExpansion
-  , formatRecord
-  , dump
-  , makeDefaultKeyword
   -- * Expansion Operations
 
   -- $expansion
-  , expandHandler
+    expandHandler
   , replaceHandler
+
   -- * Knowledge Base CRUD
   , addHandler
   , updateHandler
   , deleteHandler
   , listHandler
   ) where
-import qualified Data.ByteString.Lazy   as BL (readFile, writeFile)
-import           Data.Csv
-    ( decodeByName
-    , encodeDefaultOrderedByName
+
+import           Data.List             (intercalate)
+import           Data.Maybe            (fromMaybe)
+import           LibCli.HandlerUtils
+    ( doExpansion
+    , dump
+    , formatRecord
+    , loadInput
+    , loadKb
+    , makeDefaultKeyword
+    , returnOutput
     )
-import           Data.List              (intercalate)
-import           Data.Maybe             (fromMaybe)
-import           LibCli.Adapters        (getKnowledgeBase, mapKeywordPair)
-import           LibCli.OutputInterface (returnOutput)
-import           LibCore.Decoder        as D (decode)
 import           LibCore.KnowledgeBase
     ( KnowledgeBaseStructure
     , add
@@ -47,16 +43,8 @@ import           LibCore.KnowledgeBase
     , put
     , remove
     )
-import           LibCore.Mapper         as M (mapParseStructure)
-import           LibCore.Models         (Error (..), Keyword (..))
-import           LibCore.Parser         as P (doParse)
-import           System.Directory       (doesFileExist)
-import           System.IO.Strict       as SIS (readFile)
-
-
--- TODO: General improvements (tech debt):
---  [ ] refactor duplication
---  [ ] try to use nice template for handlers to make them shorter
+import           LibCore.Models        (Error (..))
+import           System.Directory      (doesFileExist)
 
 {- $intro
 
@@ -89,64 +77,9 @@ What precedes is the following:
 
 -}
 
-
----------------
--- Utilities --
----------------
-
--- | Function to load the Knowledge Base from the specified file.
--- Supports only CSV files.
-loadKb :: FilePath -> IO (Either Error KnowledgeBaseStructure)
-loadKb fp = do
-  csvData <- BL.readFile fp
-  case decodeByName csvData of
-    Left  s      -> return $ Left $ StandardError s
-    Right (_, v) -> return $ Right $ getKnowledgeBase v
-
--- | Function to load the abbreviation input file.
--- Can be any file with contents loadable as String.
-loadInput :: FilePath -> IO (Either Error String)
-loadInput fp = do
-  s <- SIS.readFile fp
-  return $ Right s
-
--- | Performs the expansion logic on the provided string.
-doExpansion :: KnowledgeBaseStructure -> String -> IO String
-doExpansion kb s = do
-  return $ D.decode $ M.mapParseStructure kb $ P.doParse s
-
--- | Pretty prints the Keyword pair (key, value) from the Knowledge Base
-formatRecord :: (Keyword, Keyword) -> String
-formatRecord (Keyword kk kpl, Keyword vk vpl) =
-  "Key: "
-    ++ kk
-    ++ (if kpl then "(plural)" else "")
-    ++ " --> "
-    ++ "Value: "
-    ++ vk
-    ++ if vpl then "(plural)" else ""
-
--- | Dumps the Knowledge Base to the specified file path.
--- Writes the KB out in CSV format.
-dump :: FilePath -> KnowledgeBaseStructure -> IO ()
-dump kb_fp kb = do
-  let entries = map mapKeywordPair $ listAll kb
-  BL.writeFile kb_fp $ encodeDefaultOrderedByName entries
-
-
--- | Embeds the string in a keyword.
--- By default assigns `False` to the plural attribute.
---
--- Examples:
---
--- >>> makeDefaultKeyword "hello"
--- Keyword {keyword = "hello", plural = False}
-makeDefaultKeyword :: String -> Keyword
-makeDefaultKeyword s = Keyword { keyword = s, plural = False }
-
---------------------------
--- Expansion Operations --
---------------------------
+-- TODO: General improvements (tech debt):
+--  [ ] refactor duplication
+--  [ ] try to use nice template for handlers to make them shorter
 
 -- | Expand command handler.
 -- Deal with single input expansion without reading an input file.
@@ -321,7 +254,3 @@ listHandler kb_mfp = do
     lkb <- loadKb kb_fp
     let rs = map formatRecord . listAll <$> lkb
     return $ intercalate "\n" <$> rs
-
-
--- TODO(tech debt): add get command to Spec
--- get
