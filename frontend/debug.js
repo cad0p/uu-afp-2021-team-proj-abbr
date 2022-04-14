4ce8e3cb3835852b5f66a6fefa480f366b6de8fe
Dmitrii Orlov <dmtree.dev@yahoo.com>
2022-04-14 21:12:33 +0200

    Some comments on debug.js

--- frontend/debug.js
+++ frontend/debug.js
@@ -1,3 +1,8 @@
+/**
+ * This module is used for manual experiments and
+ * demonstration of the JS->Elm interaction.
+ */
+
 const { setupApps } = require("./wiring/Apps");
 
 const { expandApp } = setupApps({ kbPath: ".demo/data/example_kb.csv" });
@@ -7,12 +12,15 @@ const app = expandApp;
 
 // Setup subscriptions.
 app.ports.toExtensionInfo.subscribe(function (msg) {
+  // Define message handler.
   console.debug("INFO: got from port", msg);
 });
 app.ports.toExtensionError.subscribe(function (msg) {
+  // Define message handler.
   console.error("ERR: got from port", msg);
 });
 app.ports.toExtensionContent.subscribe(function (msg) {
+  // Define message handler.
   console.info("CONTENT: got from port ->", msg);
 });
 
@@ -20,3 +28,7 @@ app.ports.toExtensionContent.subscribe(function (msg) {
 console.log("Sending to Elm...");
 app.ports.fromExtension.send("Ping!");
 app.ports.fromExtensionExpand.send("@@hl");
+// Output:
+// > Sending to Elm...
+// > INFO: got from port Pong!
+// > CONTENT: got from port -> hello
